11e9935d32536a30ef0912089108bd92efe44d3d
VunboYao <yaoyuanbo@lansijie.com>
2023-04-12 11:20:28 +0800

    fix: 修复代码格式化

--- src/const/symbolKey.ts
+++ src/const/symbolKey.ts
@@ -1,9 +1,9 @@
-import type { InjectionKey } from 'vue'
-
 /**
  * @file symbolKey.js
  * @author VunboYao
  * @date 2023/4/11 15:48
  */
+import type { InjectionKey } from 'vue'
+
 export const key = Symbol('demo') as InjectionKey<string>
 export const noProvideKey = Symbol('noValue') as InjectionKey<string>
