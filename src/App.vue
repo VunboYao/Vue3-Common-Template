65a46dce4404cb2bdc00aaef82515b5838a36064
VunboYao <yaoyuanbo@lansijie.com>
2023-07-03 15:01:01 +0800

    feat: 依赖更新

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <!-- <HelloWorld /> -->
-  <CreateCalendar />
+  <!--  <HelloWorld />-->
+  <!--  <CreateCalendar />-->
+  <ReviewVue />
 </template>
 
 <script setup lang="ts">
 // import HelloWorld from '@/components/AntVue.vue'
 // import CreateCalendar from '@/components/CreateCalendar'
-import CreateCalendar from '@/components/CreateCalendar/CreateCalendar.vue'
+// import CreateCalendar from '@/components/CreateCalendar/CreateCalendar.vue'
+import ReviewVue from '@/components/ReviewVue.vue'
 </script>
 
 <style lang="less">
---
+++ src/components/ReviewVue.vue
@@ -0,0 +1,17 @@
+<!--
+  @file ReviewVue
+  @author VunboYao
+  @date 2023/7/3 14:42
+-->
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>Hello</div>
+</template>
+
+<style scoped lang="less">
+
+</style>
+
