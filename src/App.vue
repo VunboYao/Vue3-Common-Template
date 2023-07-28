e147a9636465c0993c5f91ffb1bc93bc4c5b6d16
VunboYao <vunbo@outlook.com>
2023-07-28 17:17:44 +0800

    feat: 依赖版本更新

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <!--  <HelloWorld />-->
-  <!--  <CreateCalendar />-->
-  <ReviewVue />
+  <div>
+    <HelloWorld ref="modal" foo="Ok" bar="12" />
+  </div>
 </template>
-
 <script setup lang="ts">
-// import HelloWorld from '@/components/AntVue.vue'
-// import CreateCalendar from '@/components/CreateCalendar'
-// import CreateCalendar from '@/components/CreateCalendar/CreateCalendar.vue'
-import ReviewVue from '@/components/ReviewVue.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import { key } from '@/const/symbolKey'
+
+import {
+  ref, reactive, computed
+} from 'vue'
+
+provide(key, 'Hello Boy')
+const arr = [1, 2, 3]
+const modal = ref<InstanceType<typeof HelloWorld>>()
+onMounted(() => {
+  setTimeout(() => {
+    modal.value?.open()
+  }, 2000)
+})
+const obj2 = {
+  foo: 123
+}
+
 </script>
 
 <style lang="less">
+div {
+  ul > li {
+    display: flex;
+    order: 1;
+    width: 200px;
+    height: 200px;
+    margin: 10px;
+    color: darkblue;
+    list-style-type: none;
+    pointer-events: all;
+    cursor: pointer;
+    background: #777;
+    border: 1px solid #CCC;
+    box-shadow: antiquewhite;
+  }
+}
 </style>
